0b07d16bb10ab54dc2f7fc913b75f1ebe3957203
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-26 21:21:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,11 +26,18 @@ get '/say/:word1/:word2/:word3/:word4/:word5'do
 end
 
 get '/:operation/:number1/:number2' do
-@number1,@number2 = params[:number1].to_i, params[:number2].to_i
-"#{@number1 + @number2}"
-"#{@number1 + (- @number2)}"
-"#{@number1 * (@number2)}"
-"#{@number1 % (@number2)}"
-
-end
+ +    operation = params[:operation]
+ +    num1 = params[:number1].to_i
+ +    num2 = params[:number2].to_i
+ +    case operation
+ +    when "add"
+ +      "#{num1+num2}"
+ +    when "subtract"
+ +      "#{num1-num2}"
+ +    when "multiply"
+ +      "#{num1*num2}"
+ +    when "divide"
+ +      "#{num1/num2}"
+ +    end
+ +  end
 end
