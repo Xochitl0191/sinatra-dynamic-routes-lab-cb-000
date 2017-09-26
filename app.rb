3e1ed8dd0d988821ef6cdd219626dd6ec628cc38
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-26 21:21:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,11 +14,11 @@ number = params[:number]
 end
 
 get '/say/:number/:phrase' do
-  @num = params[:num].to_i
-  @phrase = params[:phrase] + "\n"
-
-  "#{@phrase * @num}"
-end
+  get '/say/:number/:phrase' do
+     num = params[:number].to_i
+    phrase = params[:phrase]
+     "#{phrase}\n"*num
+   end
 
 get '/say/:word1/:word2/:word3/:word4/:word5'do
  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
