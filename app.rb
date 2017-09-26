53e80c8582db5542235d26522412dfbc285f9a16
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-26 21:21:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ number = params[:number]
 (number.to_i**2).to_s
 end
 
-get '/say/:number/:phrase' do
+
   get '/say/:number/:phrase' do
      num = params[:number].to_i
     phrase = params[:phrase]
